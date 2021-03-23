cf59d0b026c741652245f641a9630ac84d4467ef
Raphael Koszalka <ctl_rkoszalka@uolinc.com>
2021-03-23 09:10:46 -0300

    feat(data-bureau-transactions) : cassandra configuration

---
+++ src/main/java/org/koszalka/cassandra/bureau/config/TransactionsCassandraConfig.java
@@ -0,0 +1,46 @@
+package org.koszalka.cassandra.bureau.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.cassandra.config.AbstractCassandraConfiguration;
+import org.springframework.data.cassandra.config.SchemaAction;
+import org.springframework.data.cassandra.repository.config.EnableCassandraRepositories;
+
+@Configuration
+@EnableCassandraRepositories
+public class TransactionsCassandraConfig extends AbstractCassandraConfiguration {
+
+    @Value("${cassandra.contactpoints}")
+    private String contactPoints;
+    @Value("${cassandra.port}")
+    private int port;
+    @Value("${cassandra.keyspace}")
+    private String keySpace;
+    @Value("${cassandra.basePackages}")
+    private String basePackages;
+
+    @Override
+    protected String getKeyspaceName() {
+        return keySpace;
+    }
+
+    @Override
+    protected String getContactPoints() {
+        return contactPoints;
+    }
+
+    @Override
+    protected int getPort() {
+        return port;
+    }
+
+    @Override
+    public SchemaAction getSchemaAction() {
+        return SchemaAction.CREATE_IF_NOT_EXISTS;
+    }
+
+    @Override
+    public String[] getEntityBasePackages() {
+        return new String[] {basePackages};
+    }
+}
---
+++ src/main/java/org/koszalka/cassandra/bureau/persistence/entity/TransactionEntity.java
@@ -0,0 +1,30 @@
+package org.koszalka.cassandra.bureau.persistence.entity;
+
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.data.cassandra.core.mapping.PrimaryKey;
+import org.springframework.data.cassandra.core.mapping.Table;
+
+@Table
+@Getter
+@Setter
+public class TransactionEntity {
+
+    @PrimaryKey
+    private final String id;
+    private final String name;
+    private final int age;
+
+    public TransactionEntity(String id, String name, int age) {
+        this.id = id;
+        this.name = name;
+        this.age = age;
+    }
+
+    @Override
+    public String toString() {
+        return String.format("{ @type = %1$s, id = %2$s, name = %3$s, age = %4$d }", getClass().getName(), getId(),
+                getName(), getAge());
+    }
+
+}
